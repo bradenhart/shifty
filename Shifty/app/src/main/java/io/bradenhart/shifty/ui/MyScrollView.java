7faf8c31c9c76fd241ddf7f65e154ace8b15f254
BRADEN HART <bradendhart@gmail.com>
2017-05-02 23:34:08 +1200

    created custom scrollview class

---
+++ Shifty/app/src/main/java/io/bradenhart/shifty/ui/MyScrollView.java
@@ -0,0 +1,66 @@
+package io.bradenhart.shifty.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+/**
+ * Created by bradenhart on 11/03/17.
+ */
+
+public class MyScrollView extends ScrollView {
+
+    private int positionY = 0;
+    private Runnable scrollRunnable;
+    private int newCheck = 50;
+    private int height;
+    private int childCount;
+
+    public interface OnScrollStoppedListener{
+        void onScrollStopped();
+    }
+
+    private OnScrollStoppedListener onScrollStoppedListener;
+
+    public MyScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        scrollRunnable = new Runnable() {
+
+            public void run() {
+                int newPosition = getScrollY();
+                if (positionY - newPosition == 0) { //has stopped
+                    if (onScrollStoppedListener != null) {
+
+                        onScrollStoppedListener.onScrollStopped();
+                    }
+                } else {
+                    positionY = getScrollY();
+                    MyScrollView.this.postDelayed(scrollRunnable, newCheck);
+                }
+            }
+        };
+    }
+
+    public void setOnScrollStoppedListener(MyScrollView.OnScrollStoppedListener listener){
+        onScrollStoppedListener = listener;
+    }
+
+    public void startScrollerTask(){
+        positionY = getScrollY();
+        MyScrollView.this.postDelayed(scrollRunnable, newCheck);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        height = getChildAt(0).getMeasuredHeight();
+        childCount = ((LinearLayout) getChildAt(0)).getChildCount();
+
+    }
+
+    public int getUnitHeight() {
+        return height / childCount;
+    }
+}
