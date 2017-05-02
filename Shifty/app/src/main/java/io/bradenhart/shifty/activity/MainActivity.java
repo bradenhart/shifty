ad2c2e02231514b7019fb51f292f15c0caef8975
BRADEN HART <bradendhart@gmail.com>
2017-05-02 16:13:58 +1200

    created project and activity to display shifts

---
+++ Shifty/app/src/main/java/io/bradenhart/shifty/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package io.bradenhart.shifty.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.bradenhart.shifty.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
