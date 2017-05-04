2a8dba7567b84e4168a26b22d14e40e7d71d7331
BRADEN HART <bradendhart@gmail.com>
2017-05-04 20:25:51 +1200

    tidied up test data

--- Shifty/app/src/main/java/io/bradenhart/shifty/database/TestData.java
+++ Shifty/app/src/main/java/io/bradenhart/shifty/database/TestData.java
@@ -22,54 +22,55 @@ public class TestData {
     private static int y = 2017;
 
     public static Shift[] shifts = new Shift[] {
-//            new Shift(new ShiftDate(29, WEDNESDAY, MARCH, y), new ShiftTime(NINE, THIRTY, AM), new ShiftTime(SIX, ZERO, PM)),
-//            new Shift(new ShiftDate(31, FRIDAY, MARCH, y), new ShiftTime(SIX, ZERO, PM), new ShiftTime(EIGHT, ZERO, PM)),
-//            new Shift(new ShiftDate(1, SATURDAY, APRIL, y), new ShiftTime(SIX, ZERO, PM), new ShiftTime(EIGHT, ZERO, PM)),
-//            new Shift(new ShiftDate(2, SUNDAY, APRIL, y), new ShiftTime(NINE, ZERO, AM), new ShiftTime(FOUR, ZERO, PM)),
-//
-//            new Shift(new ShiftDate(5, WEDNESDAY, APRIL, y), new ShiftTime(NINE, ZERO, AM), new ShiftTime(ShiftTime.Hour.FIVE, THIRTY, PM)),
-//            new Shift(new ShiftDate(9, SUNDAY, APRIL, y), new ShiftTime(NINE, ZERO, AM), new ShiftTime(FOUR, ZERO, PM)),
-//
-//            new Shift(new ShiftDate(10, MONDAY, APRIL, y), new ShiftTime(NINE, THIRTY, AM), new ShiftTime(SIX, ZERO, PM)),
-//            new Shift(new ShiftDate(12, WEDNESDAY, APRIL, y), new ShiftTime(NINE, THIRTY, AM), new ShiftTime(SIX, ZERO, PM)),
-//            new Shift(new ShiftDate(13, THURSDAY, APRIL, y), new ShiftTime(NINE, THIRTY, AM), new ShiftTime(SIX, ZERO, PM)),
-//            new Shift(new ShiftDate(15, SATURDAY, APRIL, y), new ShiftTime(NINE, ZERO, AM), new ShiftTime(ShiftTime.Hour.FIVE, THIRTY, PM)),
-//
-//            new Shift(new ShiftDate(17, MONDAY, APRIL, y), new ShiftTime(NINE, ZERO, AM), new ShiftTime(FOUR, ZERO, PM)),
-//            new Shift(new ShiftDate(18, TUESDAY, APRIL, y), new ShiftTime(NINE, THIRTY, AM), new ShiftTime(SIX, ZERO, PM)),
-//            new Shift(new ShiftDate(19, WEDNESDAY, APRIL, y), new ShiftTime(NINE, THIRTY, AM), new ShiftTime(SIX, ZERO, PM)),
-//            new Shift(new ShiftDate(21, FRIDAY, APRIL, y), new ShiftTime(EIGHT, ZERO, AM), new ShiftTime(FOUR, THIRTY, PM)),
-//            new Shift(new ShiftDate(23, SUNDAY, APRIL, y), new ShiftTime(NINE, ZERO, AM), new ShiftTime(FOUR, ZERO, PM))
-//            new Shift("2017-03-29 09:30:00 AM", "2017-03-31 06:00:00 PM")
+            // 27 March
             new Shift(y, Calendar.MARCH, 29, 9, 30, 0, 6, 0, 1),
             new Shift(y, Calendar.MARCH, 31, 6, 0, 1, 8, 0, 1),
             new Shift(y, Calendar.APRIL, 1, 6, 0, 1, 8, 0, 1),
             new Shift(y, Calendar.APRIL, 2, 9, 0, 0, 4, 0, 1),
 
+            // 3 April
             new Shift(y, Calendar.APRIL, 5, 9, 0, 0, 5, 30, 1),
             new Shift(y, Calendar.APRIL, 9, 9, 0, 0, 4, 0, 1),
 
+            // 10 April
             new Shift(y, Calendar.APRIL, 10, 9, 30, 0, 6, 0, 1),
             new Shift(y, Calendar.APRIL, 12, 9, 30, 0, 6, 0, 1),
             new Shift(y, Calendar.APRIL, 13, 9, 30, 0, 6, 0, 1),
             new Shift(y, Calendar.APRIL, 15, 9, 0, 0, 5, 30, 1),
 
+            // 17 April
             new Shift(y, Calendar.APRIL, 17, 9, 0, 0, 4, 0, 1),
             new Shift(y, Calendar.APRIL, 18, 9, 30, 0, 6, 0, 1),
             new Shift(y, Calendar.APRIL, 19, 9, 30, 0, 6, 0, 1),
             new Shift(y, Calendar.APRIL, 21, 8, 0, 0, 4, 30, 1),
             new Shift(y, Calendar.APRIL, 23, 9, 0, 0, 4, 0, 1),
 
+            // 24 April
             new Shift(y, Calendar.APRIL, 24, 9, 30, 0, 6, 0, 1),
             new Shift(y, Calendar.APRIL, 26, 8, 0, 0, 4, 30, 1),
             new Shift(y, Calendar.APRIL, 30, 9, 0, 0, 4, 0, 1),
 
+            // 1 May
             new Shift(y, Calendar.MAY, 1, 8, 0, 0, 4, 30, 1),
             new Shift(y, Calendar.MAY, 3, 9, 30, 0, 6, 0, 1),
             new Shift(y, Calendar.MAY, 7, 9, 0, 0, 4, 0, 1),
 
+            // 8 May
             new Shift(y, Calendar.MAY, 9, 9, 30, 0, 6, 0, 1),
             new Shift(y, Calendar.MAY, 14, 9, 0, 0, 4, 0, 1)
+
+            /* fake data */
+            // 15 May
+            ,new Shift(y, Calendar.MAY, 15, 8, 0, 0, 4, 30, 1),
+            new Shift(y, Calendar.MAY, 17, 9, 30, 0, 6, 0, 1),
+
+            // 22 May
+            new Shift(y, Calendar.MAY, 23, 9, 0, 0, 5, 30, 1),
+            new Shift(y, Calendar.MAY, 24, 8, 0, 0, 4, 30, 1),
+            new Shift(y, Calendar.MAY, 26, 8, 30, 0, 5, 0, 1),
+
+            // 29 May
+            new Shift(y, Calendar.MAY, 30, 9, 30, 0, 6, 0, 1)
     };
 
     public static void addDataToDB(Context context) {
