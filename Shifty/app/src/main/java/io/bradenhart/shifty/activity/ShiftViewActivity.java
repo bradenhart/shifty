e13760dc664ce5c122a324742b1521f9b14711fe
BRADEN HART <bradendhart@gmail.com>
2017-05-04 13:41:37 +1200

    implemented loading more workweeks from db
    floating action button appears when bottom of recycler view is visible.
    clicking button loads one more week and displays it.
    hide button when no more weeks can be loaded and when scrolling.

--- Shifty/app/src/main/java/io/bradenhart/shifty/activity/ShiftViewActivity.java
+++ Shifty/app/src/main/java/io/bradenhart/shifty/activity/ShiftViewActivity.java
@@ -2,6 +2,7 @@ package io.bradenhart.shifty.activity;
 
 import android.content.Intent;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -9,6 +10,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,19 +28,16 @@ import butterknife.BindDimen;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import butterknife.Optional;
 import io.bradenhart.shifty.R;
 import io.bradenhart.shifty.adapter.MySectionAdapter;
 import io.bradenhart.shifty.database.DatabaseManager;
-import io.bradenhart.shifty.database.ShiftrContract;
 import io.bradenhart.shifty.database.TestData;
 import io.bradenhart.shifty.domain.Shift;
 import io.bradenhart.shifty.domain.WorkWeek;
 import io.bradenhart.shifty.util.DateUtil;
 import io.bradenhart.shifty.view.WorkWeekSection;
-import io.github.luizgrp.sectionedrecyclerviewadapter.SectionedRecyclerViewAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class ShiftViewActivity extends AppCompatActivity {
 
     final String TAG = "MainActivity.class";
     private final String appName = "Shifty";
@@ -48,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
     TextView titleView;
     @BindView(R.id.rv_shift_list)
     RecyclerView recyclerView;
+    @BindView(R.id.button_load_more)
+    FloatingActionButton loadMoreButton;
     @BindView(R.id.cardview_bottom_action_bar)
     CardView bottomActionBar;
     @BindView(R.id.button_select_weeks)
@@ -59,21 +61,22 @@ public class MainActivity extends AppCompatActivity {
     @BindDimen(R.dimen.workweek_shift_progress_width)
     int progressWidth;
 
+    private int weeks = 3;
+    private int offset = 0;
+
     private MySectionAdapter sectionedAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_shiftview);
 
-        ButterKnife.bind(MainActivity.this);
+        ButterKnife.bind(ShiftViewActivity.this);
 
         // set up actionbar
-        toolbar = ButterKnife.findById(appBar, R.id.toolbar);
+        setUpActionBar();
 
-        setUpActionBar(toolbar);
-
-        String[] datetimes = DateUtil.getDateTimesForRange(3, 0);
+        String[] datetimes = DateUtil.getDateTimesForRange(weeks, offset);
 
         TestData.deleteAllTestData(getApplicationContext());
         TestData.addDataToDB(getApplicationContext());
@@ -108,13 +111,31 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(sectionedAdapter);
 //        recyclerView.scrollToPosition(6);
 
+        recyclerView.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(View view, int i, int i1, int i2, int i3) {
+                if (!recyclerView.canScrollVertically(1)) {
+//                    Log.e("SCROLL", "Can't scroll up");
+                    loadMoreButton.setVisibility(View.VISIBLE);
+                } else {
+                    if (loadMoreButton.getVisibility() == View.VISIBLE) {
+                        loadMoreButton.setVisibility(View.GONE);
+                    }
+                }
+
+//                if (!recyclerView.canScrollVertically(0)) {
+//                    Log.e("SCROLL", "Can't scroll down");
+//                }
+            }
+        });
+
     }
 
 
     /* initialisation/setup methods */
-    private void setUpActionBar(Toolbar toolbar) {
+    private void setUpActionBar() {
+        toolbar = ButterKnife.findById(appBar, R.id.toolbar);
         titleView = ButterKnife.findById(toolbar, R.id.textview_toolbar_title);
-
         // replace the default actionbar with our toolbar
         setSupportActionBar(toolbar);
         // disable the title that would appear in the actionbar
@@ -127,6 +148,46 @@ public class MainActivity extends AppCompatActivity {
 //        getSupportActionBar().setDisplayShowHomeEnabled(true);
         // set the navigation drawer icon to the hamburger icon
 //        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+    }
+
+    @OnClick(R.id.button_load_more)
+    public void clickLoadMoreButton() {
+        offset = weeks;
+        weeks += 1;
+        String[] datetimes = DateUtil.getDateTimesForRange(1, offset);
+
+//        sectionedAdapter = new SectionedRecyclerViewAdapter();
+
+        Map<String, List<Shift>> map = new DatabaseManager(getApplicationContext()).getShiftsInDateRange(datetimes);
+        SimpleDateFormat oldFmt = new SimpleDateFormat(DateUtil.FMT_DATETIME, Locale.ENGLISH);
+        SimpleDateFormat newFmt = new SimpleDateFormat("MMMM dd", Locale.ENGLISH);
+
+        if (map.keySet().size() == 0) {
+            loadMoreButton.setVisibility(View.GONE);
+        }
+
+        Toast.makeText(getApplicationContext(), map.keySet().size() + "", Toast.LENGTH_SHORT).show();
+        for (String week : map.keySet()) {
+            System.out.println("week: " + week);
+            try {
+                Date date = oldFmt.parse(week);
+                String tag = UUID.randomUUID().toString();
+                Log.e("TAG", "1... " + tag);
+                System.out.println(map.get(week));
+                WorkWeekSection workWeekSection = new WorkWeekSection.Builder()
+                        .setContext(this)
+                        .setTag(tag)
+                        .setWorkWeek(new WorkWeek("Week of " + newFmt.format(date), map.get(week)))
+                        .setAdapter(sectionedAdapter)
+                        .build();
+
+                sectionedAdapter.addSection(tag, workWeekSection);
+//                recyclerView.invalidate();
+                sectionedAdapter.notifyDataSetChanged();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @OnClick(R.id.button_select_weeks)
@@ -166,7 +227,7 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.button_new_shift)
     public void onClickNewShiftButton() {
-        Intent intent = new Intent(MainActivity.this, ShiftActivity.class);
+        Intent intent = new Intent(ShiftViewActivity.this, ShiftActivity.class);
 
         startActivity(intent);
     }
