2caa8a9520355ffb761a18602de1d81a5337b541
BRADEN HART <bradendhart@gmail.com>
2017-05-06 01:15:12 +1200

    made a test data generator, it's buggy

--- Shifty/app/src/main/java/io/bradenhart/shifty/database/TestData.java
+++ Shifty/app/src/main/java/io/bradenhart/shifty/database/TestData.java
@@ -1,11 +1,15 @@
 package io.bradenhart.shifty.database;
 
 import android.content.Context;
+import android.util.Log;
 
 import io.bradenhart.shifty.domain.Shift;
 import io.bradenhart.shifty.domain.ShiftDate.*;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
 
 import static io.bradenhart.shifty.domain.ShiftDate.Month.*;
 import static io.bradenhart.shifty.domain.ShiftDate.Weekday.*;
@@ -79,7 +83,79 @@ public class TestData {
         }
     }
 
+    public static void addDataToDB(Context context, int weeks) {
+        shifts = generateShifts(weeks);
+        addDataToDB(context);
+    }
+
     public static void deleteAllTestData(Context context) {
         new DatabaseManager(context).deleteAllShifts();
     }
+
+    private static int generateMonth() {
+        Random r = new Random();
+        Calendar c = Calendar.getInstance();
+        int month = c.get(Calendar.MONTH);
+        int val = r.nextInt(2);
+        int dir = r.nextInt(1);
+        if (month == 0 && dir == 0) return month;
+        if (dir == 0 && month - val < 0) return 0;
+        return dir == 0 ? month - val : month + 1;
+    }
+
+    private static int generateStartHour() {
+        // 6 -> 11
+        // (0 -> 5) + 6
+        return new Random().nextInt(5) + 6;
+    }
+
+    private static int generateEndHour() {
+        // 1 -> 8
+        // (0 -> 7) + 1
+        return new Random().nextInt(7);
+    }
+
+    private static int generateMinute() {
+        // 00 -> 55
+        // (0 -> 11) * 5
+        return new Random().nextInt(11) * 5;
+    }
+
+    private static int generateDayOfMonth(int month) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, y);
+        c.set(Calendar.MONTH, month);
+        int max = c.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        return new Random().nextInt(max);
+    }
+
+    private static int generateMaxShiftsInWeek() {
+        return new Random().nextInt(7) + 1;
+    }
+
+    public static Shift[] generateShifts(int weeks) {
+        List<Shift> shifts = new ArrayList<>();
+
+        for (int w = 0; w < weeks; w++) {
+            int maxShifts = generateMaxShiftsInWeek();
+            for (int d = 1; d <= maxShifts; d++) {
+                int month = generateMonth();
+                int startHour = generateStartHour();
+                int endHour = generateEndHour();
+                int startMin = generateMinute();
+                int endMin = generateMinute();
+                int day = generateDayOfMonth(month);
+                shifts.add(new Shift(y, month, day, startHour, startMin, 0, endHour, endMin, 1));
+                String shift = "Shift(" + y + ", " + month + ", " + day + ", " + startHour + ", "
+                        + startMin + ", 0, " + endHour + ", " + endMin + ", 1)";
+                Log.e("GEN", shift);
+            }
+        }
+
+        Shift[] s = new Shift[shifts.size()];
+        shifts.toArray(s);
+        return s;
+    }
+
 }
