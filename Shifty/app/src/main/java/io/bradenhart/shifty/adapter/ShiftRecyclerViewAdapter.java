2178d5693848c465bd76ec7bd12c1be61d9a3b9c
BRADEN HART <bradendhart@gmail.com>
2017-05-19 01:02:24 +1200

    began changing Shift adapter to display shifts from cursor

--- Shifty/app/src/main/java/io/bradenhart/shifty/adapter/ShiftRecyclerViewAdapter.java
+++ Shifty/app/src/main/java/io/bradenhart/shifty/adapter/ShiftRecyclerViewAdapter.java
@@ -2,6 +2,7 @@ package io.bradenhart.shifty.adapter;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.database.Cursor;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -22,6 +23,7 @@ import butterknife.OnClick;
 import io.bradenhart.shifty.R;
 import io.bradenhart.shifty.activity.ShiftActivity;
 import io.bradenhart.shifty.data.DatabaseManager;
+import io.bradenhart.shifty.data.ShiftyContract;
 import io.bradenhart.shifty.domain.Shift;
 import io.bradenhart.shifty.util.DateUtil;
 import io.bradenhart.shifty.util.Utils;
@@ -37,12 +39,34 @@ public class ShiftRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.
     private int itemHeight, progressWidth;
     private WorkWeekRecyclerViewAdapter parentAdapter;
     private int parentPos;
+    private Cursor cursor;
 
     public ShiftRecyclerViewAdapter(Context context, List<Shift> shifts) {
         this.context = context;
         this.shifts = shifts;
         this.itemHeight = context.getResources().getDimensionPixelSize(R.dimen.workweek_item_height);
         this.progressWidth = context.getResources().getDimensionPixelSize(R.dimen.workweek_shift_progress_width);
+    }
+
+    public ShiftRecyclerViewAdapter(Context context, String datetime) {
+        this.context = context;
+        this.cursor = getShiftsInWeek(datetime);
+        this.itemHeight = context.getResources().getDimensionPixelSize(R.dimen.workweek_item_height);
+        this.progressWidth = context.getResources().getDimensionPixelSize(R.dimen.workweek_shift_progress_width);
+    }
+
+    private Cursor getShiftsInWeek(String weekDate) {
+        // get the ISO8601 formatted string for Monday 00:00 of the current week
+        String selection = ShiftyContract.Shift.COLUMN_WEEK_START_DATETIME + " = ?";
+        String[] selectionArgs = new String[] { weekDate };
+
+        return context.getContentResolver().query(
+                ShiftyContract.Shift.CONTENT_URI, // query Shift table (/shift)
+                null, // get all columns
+                selection, // get all shifts from this week onwards
+                selectionArgs,
+                ShiftyContract.Shift.COLUMN_SHIFT_START_DATETIME + " asc" // order by shift start time, earliest to latest
+        );
     }
 
     public void removeShift(int pos) {
@@ -67,24 +91,41 @@ public class ShiftRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         final ShiftViewHolder shiftHolder = (ShiftViewHolder) holder;
 
-        final Shift shift = shifts.get(position);
+        cursor.moveToPosition(position);
+        // get column index values
+        int idCol = cursor.getColumnIndex(ShiftyContract.Shift._ID);
+        int shiftStartCol = cursor.getColumnIndex(ShiftyContract.Shift.COLUMN_SHIFT_START_DATETIME);
+        int shiftEndCol = cursor.getColumnIndex(ShiftyContract.Shift.COLUMN_SHIFT_END_DATETIME);
 
-        shiftHolder.shift = shift;
-        shiftHolder.dayOfMonthTV.setText(DateUtil.getDayOfMonth(shift.getId()));
-        shiftHolder.dayOfWeekTV.setText(DateUtil.getWeekday(shift.getId(), DateUtil.FMT_WEEKDAY_FULL));
-        shiftHolder.startTimeTV.setText(shift.getStartTime().toString());
-        shiftHolder.endTimeTV.setText(shift.getEndTime().toString());
+        // get values from cursor
+        String shiftStart = cursor.getString(shiftStartCol);
+        String shiftEnd = cursor.getString(shiftEndCol);
 
-        shiftHolder.shiftLengthTV.setText(String.format(Locale.ENGLISH, "%.2f hrs", shift.getPaidHours()));
+        String dayOfMonth = DateUtil.getDayOfMonth(shiftStart, DateUtil.FMT_ISO_8601_DATETIME);
+        String dayOfWeek = DateUtil.getWeekday(shiftStart, DateUtil.FMT_ISO_8601_DATETIME, DateUtil.FMT_WEEKDAY_FULL);
+        String startTime = DateUtil.getTime(shiftStart, DateUtil.FMT_ISO_8601_DATETIME, DateUtil.FMT_TIME_SHORT);
+        String endTime = DateUtil.getTime(shiftEnd, DateUtil.FMT_ISO_8601_DATETIME, DateUtil.FMT_TIME_SHORT);
 
-        double progress = DateUtil.getShiftProgress(shift);
+        shiftHolder.view.setTag(cursor.getLong(idCol));
+        shiftHolder.dayOfMonthTV.setText(dayOfMonth);
+        shiftHolder.dayOfWeekTV.setText(dayOfWeek);
+        shiftHolder.startTimeTV.setText(startTime);
+        shiftHolder.endTimeTV.setText(endTime);
+//
+        double shiftLength = DateUtil.getHoursBetween(shiftStart, shiftEnd, DateUtil.FMT_ISO_8601_DATETIME);
+        double paidHours = shiftLength <= 5.0 ? shiftLength : shiftLength - 0.5;
+        
+        shiftHolder.paidHoursTV.setText(String.format(Locale.ENGLISH, "%.2f hrs", paidHours));
+
+        double progress = DateUtil.getShiftProgress(shiftStart, shiftEnd, DateUtil.FMT_ISO_8601_DATETIME);
+        Log.e("progress", startTime + "   "  + progress);
         int percentHeight = (int) Math.ceil(itemHeight * progress);
         shiftHolder.shiftProgressBar.setLayoutParams(new LinearLayout.LayoutParams(progressWidth, percentHeight));
     }
 
     @Override
     public int getItemCount() {
-        return shifts.size();
+        return cursor.getCount();
     }
 
 
@@ -92,6 +133,7 @@ public class ShiftRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.
 
         private Context context;
         Shift shift;
+        View view;
         @BindView(R.id.layout_shift_item)
         LinearLayout itemLayout;
         @BindView(R.id.textview_day_of_month)
@@ -109,8 +151,8 @@ public class ShiftRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.
         LinearLayout optionsLayout;
         @BindView(R.id.button_edit_shift)
         ImageButton editButton;
-        @BindView(R.id.textview_shift_length)
-        TextView shiftLengthTV;
+        @BindView(R.id.textview_paid_hours)
+        TextView paidHoursTV;
         @BindView(R.id.button_delete_shift)
         ImageButton deleteButton;
         @BindView(R.id.button_close_options)
@@ -119,12 +161,13 @@ public class ShiftRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.
         public ShiftViewHolder(Context context, View itemView) {
             super(itemView);
             this.context = context;
+            this.view = itemView;
             ButterKnife.bind(this, itemView);
         }
 
         @OnClick(R.id.layout_shift_item)
         void onClickItemLayout() {
-            Log.e("shift id: ", shift.getId());
+            Log.e("shift _id: ", view.getTag() + "");
             if (optionsLayout.getVisibility() == View.GONE) {
                 optionsLayout.setVisibility(View.VISIBLE);
             }
