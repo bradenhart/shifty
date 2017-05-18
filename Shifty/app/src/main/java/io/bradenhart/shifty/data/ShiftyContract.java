1c828dc79097d1dd1c8622bc62a15c3a71a10784
BRADEN HART <bradendhart@gmail.com>
2017-05-18 19:40:07 +1200

    added string and Uri constants to ShiftyContract for Content Provider to use

--- Shifty/app/src/main/java/io/bradenhart/shifty/data/ShiftyContract.java
+++ Shifty/app/src/main/java/io/bradenhart/shifty/data/ShiftyContract.java
@@ -1,5 +1,6 @@
 package io.bradenhart.shifty.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -8,14 +9,35 @@ import android.provider.BaseColumns;
 
 public class ShiftyContract {
 
-    private ShiftyContract() {}
+    public static final String CONTENT_AUTHORITY = "io.bradenhart.shifty";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_SHIFT = "shift";
+    public static final String PATH_WORKWEEK = "workweek";
+
+    private ShiftyContract() {
+    }
 
     public static class Shift implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_SHIFT)
+                .build();
+
         public static final String TABLE_NAME = "Shift";
         public static final String COLUMN_WEEK_START_DATETIME = "week_start_datetime";
         public static final String COLUMN_WEEK_END_DATETIME = "week_end_datetime";
         public static final String COLUMN_SHIFT_START_DATETIME = "shift_start_datetime";
         public static final String COLUMN_SHIFT_END_DATETIME = "shift_end_datetime";
+    }
+
+    public static class Workweek {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_WORKWEEK)
+                .build();
+
     }
 
 }
