ff557763b418602dbb74744f81af35646c7b9e11
BRADEN HART <bradendhart@gmail.com>
2017-06-12 14:46:41 +1200

    replaced enums in Payslip class with @intdef annotations and interfaces

--- Shifty/app/src/main/java/io/bradenhart/shifty/domain/Payslip.java
+++ Shifty/app/src/main/java/io/bradenhart/shifty/domain/Payslip.java
@@ -1,12 +1,45 @@
 package io.bradenhart.shifty.domain;
 
+import android.support.annotation.IntDef;
+import android.support.annotation.StringDef;
+
 import java.io.Serializable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
- * Created by bradenhart on 4/04/17.
+ * Represents a payslip for a workweek.
+ *
+ * @author bradenhart
  */
-
 public class Payslip implements Serializable {
+
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({TAX_BRACKET_ONE, TAX_BRACKET_TWO, TAX_BRACKET_THREE, TAX_BRACKET_FOUR})
+    public @interface TaxBracket {}
+
+    /* TaxRate constants */
+    public static final int TAX_BRACKET_ONE = 1;
+    public static final int TAX_BRACKET_TWO = 2;
+    public static final int TAX_BRACKET_THREE = 3;
+    public static final int TAX_BRACKET_FOUR = 4;
+
+
+    @Retention(RetentionPolicy.SOURCE)
+    @StringDef({MODE_HOUR, MODE_GROSS})
+    public @interface Mode {}
+
+    /* Mode constants */
+    public static final String MODE_HOUR = "MODE_HOUR";
+    public static final String MODE_GROSS = "MODE_GROSS";
+
+    @Retention(RetentionPolicy.SOURCE)
+    @StringDef({STAFF_EMPLOYEE, STAFF_EMPLOYER})
+    public @interface Staff {}
+
+    /* Staff constants */
+    public static final String STAFF_EMPLOYEE = "STAFF_EMPLOYEE";
+    public static final String STAFF_EMPLOYER = "STAFF_EMPLOYER";
 
     /**
      * CONSTANTS
@@ -20,76 +53,17 @@ public class Payslip implements Serializable {
     private final Double HOURLY_RATE_FULL = HOURLY_RATE_BASE * (1 + HOURLY_RATE_HOLIDAY);
 
     /**
-     * ENUMS
-     */
-    private enum TaxRate {
-
-        BRACKET_ONE(1, 10.5, 0.0, 14000.0, "Up to $14,000"),
-        BRACKET_TWO(2, 17.5, 14000.0, 48000.0, "Over $14,000 and up to $48,000"),
-        BRACKET_THREE(3, 30.0, 48000.0, 70000.0, "Over $48,000 and up to $70,000"),
-        BRACKET_FOUR(4, 33.0, 70000.0, Double.POSITIVE_INFINITY, "Remaining income over $70,000");
-
-        Integer bracket;
-        Double rate;
-        Double bottom;
-        Double top;
-        String description;
-
-        TaxRate(Integer bracket, Double rate, Double bottom, Double top, String description) {
-            this.bracket = bracket;
-            this.rate = rate / 100;
-            this.bottom = bottom;
-            this.top = top;
-            this.description = description;
-        }
-
-        public Double getRate() {
-            return this.rate;
-        }
-
-        public Double getBottom() {
-            return this.bottom + 1.0;
-        }
-
-        public Integer getBracket() {
-            return this.bracket;
-        }
-
-        public Double getTop() {
-            return this.top;
-        }
-
-        public String getDescription() {
-            return this.description;
-        }
-
-        public Double getWeeklyMax() {
-            return this.top / 52;
-        }
-
-    }
-
-    public static enum Mode {
-        HOUR, GROSS
-    }
-
-    private enum Staff {
-        EMPLOYEE, EMPLOYER
-    }
-
-    /**
      * FIELDS
      */
     Double gross;
     Double hours;
     Double annual;
-    TaxRate rate;
+    @TaxBracket int taxBracket;
     Double payeTotal;
     Double kiwisavEmployee;
     Double kiwisavEmployer;
     Double studentLoan;
     Double net;
-    String weekDate;
 
     /**
      * CONSTRUCTORS
@@ -98,29 +72,28 @@ public class Payslip implements Serializable {
         this.gross = calculateGross(hours);
         this.hours = hours;
         this.annual = calculateAnnual(gross);
-        this.rate = getTaxRate(annual);
-        this.payeTotal = calculatePaye(gross, getTaxRate(annual), 0.0, rate.getBracket());
-        this.kiwisavEmployee = calculateKiwiSaver(gross, Staff.EMPLOYEE);
-        this.kiwisavEmployer = calculateKiwiSaver(gross, Staff.EMPLOYER);
+        this.taxBracket = getTaxBracket(annual);
+        this.payeTotal = calculatePaye(gross, taxBracket, 0.0);
+        this.kiwisavEmployee = calculateKiwiSaver(gross, STAFF_EMPLOYEE);
+        this.kiwisavEmployer = calculateKiwiSaver(gross, STAFF_EMPLOYER);
         this.studentLoan = calculateStudentLoan(gross);
         this.net = calculateNet(gross, payeTotal, kiwisavEmployee, studentLoan);
     }
 
-    public Payslip(Double value, Mode mode) {
-        if (mode == Mode.HOUR) {
+    public Payslip(Double value, @Mode String mode) {
+        if (mode.equals(MODE_HOUR)) {
             this.hours = value;
             this.gross = calculateGross(value);
         } else {
-            // TODO implement calculating net from gross
             this.gross = value;
             this.hours = value / HOURLY_RATE_FULL;
         }
 
         this.annual = calculateAnnual(gross);
-        this.rate = getTaxRate(annual);
-        this.payeTotal = calculatePaye(gross, getTaxRate(annual), 0.0, rate.getBracket());
-        this.kiwisavEmployee = calculateKiwiSaver(gross, Staff.EMPLOYEE);
-        this.kiwisavEmployer = calculateKiwiSaver(gross, Staff.EMPLOYER);
+        this.taxBracket = getTaxBracket(annual);
+        this.payeTotal = calculatePaye(gross, taxBracket, 0.0);
+        this.kiwisavEmployee = calculateKiwiSaver(gross, STAFF_EMPLOYEE);
+        this.kiwisavEmployer = calculateKiwiSaver(gross, STAFF_EMPLOYER);
         this.studentLoan = calculateStudentLoan(gross);
         this.net = calculateNet(gross, payeTotal, kiwisavEmployee, studentLoan);
     }
@@ -128,6 +101,60 @@ public class Payslip implements Serializable {
     /**
      * METHODS
      */
+    public Double getRate(@TaxBracket int bracket) {
+        switch (bracket) {
+            case TAX_BRACKET_ONE: return 10.5/100;
+            case TAX_BRACKET_TWO: return 17.5/100;
+            case TAX_BRACKET_THREE: return 30.0/100;
+            case TAX_BRACKET_FOUR: return 33.0/100;
+            default: return null;
+        }
+    }
+
+    public Double getBottom(@TaxBracket int bracket) {
+        Double bottom = 1.0;
+
+        switch (bracket) {
+            case TAX_BRACKET_ONE: return bottom;
+            case TAX_BRACKET_TWO: return bottom + 14000.0;
+            case TAX_BRACKET_THREE: return bottom + 30.0;
+            case TAX_BRACKET_FOUR: return bottom + 33.0;
+            default: return null;
+        }
+    }
+
+    @TaxBracket
+    public int getTaxBracket(Double annual) {
+        if (annual < 14000) {
+            return TAX_BRACKET_ONE;
+        } else if (annual < 48000) {
+            return TAX_BRACKET_TWO;
+        } else if (annual < 70000) {
+            return TAX_BRACKET_THREE;
+        } else {
+            return TAX_BRACKET_FOUR;
+        }
+    }
+
+    public Double getTop(@TaxBracket int bracket) {
+        switch (bracket) {
+            case TAX_BRACKET_ONE:
+                return 14000.0;
+            case TAX_BRACKET_TWO:
+                return 48000.0;
+            case TAX_BRACKET_THREE:
+                return 70000.0;
+            case TAX_BRACKET_FOUR:
+                return Double.POSITIVE_INFINITY;
+            default: return null;
+        }
+    }
+
+    public Double getWeeklyMax(@TaxBracket int bracket) {
+        return getTop(bracket) / 52;
+    }
+
+
     private Double calculateGross(Double hours) {
         return HOURLY_RATE_FULL * hours;
     }
@@ -136,49 +163,37 @@ public class Payslip implements Serializable {
         return gross * 52;
     }
 
-    private TaxRate getTaxRate(Double annual) {
-        if (annual < 14000) {
-            return TaxRate.BRACKET_ONE;
-        } else if (annual < 48000) {
-            return TaxRate.BRACKET_TWO;
-        } else if (annual < 70000) {
-            return TaxRate.BRACKET_THREE;
-        } else {
-            return TaxRate.BRACKET_FOUR;
-        }
-    }
-
-    private Double calculatePaye(Double gross, TaxRate rate, Double total, int n) {
+    private Double calculatePaye(Double gross, @TaxBracket int bracket, Double total) {
         Double taxedAmount;
-        Double payeRate = rate.getRate() + ACC_EARNERS_LEVY_RATE;
+        Double payeRate = getRate(bracket) + ACC_EARNERS_LEVY_RATE;
         Double payeDeduction;
         Double runningTotal;
 
-        if (rate == TaxRate.BRACKET_ONE) {
+        if (bracket == TAX_BRACKET_ONE) {
             taxedAmount = gross;
             payeDeduction = payeRate * taxedAmount;
             runningTotal = total + payeDeduction;
             return runningTotal;
-        } else if (rate == TaxRate.BRACKET_TWO) {
-            taxedAmount = gross - TaxRate.BRACKET_ONE.getWeeklyMax();
+        } else if (bracket == TAX_BRACKET_TWO) {
+            taxedAmount = gross - getWeeklyMax(TAX_BRACKET_ONE);
             payeDeduction = payeRate * taxedAmount;
             runningTotal = total + payeDeduction;
-            return calculatePaye(TaxRate.BRACKET_ONE.getWeeklyMax(), TaxRate.BRACKET_ONE, runningTotal, n - 1);
-        } else if (rate == TaxRate.BRACKET_THREE) {
-            taxedAmount = gross - TaxRate.BRACKET_TWO.getWeeklyMax();
+            return calculatePaye(getWeeklyMax(TAX_BRACKET_ONE), TAX_BRACKET_ONE, runningTotal);
+        } else if (bracket == TAX_BRACKET_THREE) {
+            taxedAmount = gross - getWeeklyMax(TAX_BRACKET_TWO);
             payeDeduction = payeRate * taxedAmount;
             runningTotal = total + payeDeduction;
-            return calculatePaye(TaxRate.BRACKET_TWO.getWeeklyMax(), TaxRate.BRACKET_TWO, runningTotal, n - 1);
+            return calculatePaye(getWeeklyMax(TAX_BRACKET_TWO), TAX_BRACKET_TWO, runningTotal);
         } else {
-            taxedAmount = gross - TaxRate.BRACKET_THREE.getWeeklyMax();
+            taxedAmount = gross - getWeeklyMax(TAX_BRACKET_THREE);
             payeDeduction = payeRate * taxedAmount;
             runningTotal = total + payeDeduction;
-            return calculatePaye(TaxRate.BRACKET_THREE.getWeeklyMax(), TaxRate.BRACKET_THREE, runningTotal, n - 1);
+            return calculatePaye(getWeeklyMax(TAX_BRACKET_THREE), TAX_BRACKET_THREE, runningTotal);
         }
     }
 
-    private Double calculateKiwiSaver(Double gross, Staff type) {
-        if (type == Staff.EMPLOYEE) {
+    private Double calculateKiwiSaver(Double gross, @Staff String type) {
+        if (type.equals(STAFF_EMPLOYEE)) {
             return gross * KIWISAVER_RATE_EMPLOYEE;
         } else {
             return gross * KIWISAVER_RATE_EMPLOYER;
@@ -235,5 +250,20 @@ public class Payslip implements Serializable {
 
     public Double getRateFull() {
         return HOURLY_RATE_FULL;
+    }
+
+    @Override
+    public String toString() {
+        return "Payslip{" +
+                "gross=" + gross +
+                ", hours=" + hours +
+                ", annual=" + annual +
+                ", taxBracket=" + taxBracket +
+                ", payeTotal=" + payeTotal +
+                ", kiwisavEmployee=" + kiwisavEmployee +
+                ", kiwisavEmployer=" + kiwisavEmployer +
+                ", studentLoan=" + studentLoan +
+                ", net=" + net +
+                '}';
     }
 }
